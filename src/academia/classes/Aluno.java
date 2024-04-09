3cd7e795c0c123168fe98b663d2395d37e2702e6
Gustavo <gv.pinheiro@outlook.com>
2024-04-09 17:04:21 -0300

    Initial commit

---
+++ src/academia/classes/Aluno.java
@@ -0,0 +1,5 @@
+package academia.classes;
+
+public class Aluno {
+
+}
---
+++ src/academia/classes/Pessoa.java
@@ -0,0 +1,39 @@
+package academia.classes;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+	private String nome;
+	private String cpf;
+	private LocalDate dataNascimento;
+	private String contato;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getContato() {
+		return contato;
+	}
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
