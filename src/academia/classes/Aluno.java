689adfacc69ebc6cb18690ff7add3a07fe09525e
Matheus Dalbone <matheusdalbone7@gmail.com>
2024-04-10 15:43:17 -0300

    add constructor class pessoa

--- src/academia/classes/Aluno.java
+++ src/academia/classes/Aluno.java
@@ -1,5 +1,16 @@
 package academia.classes;
 
-public class Aluno {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Aluno extends Pessoa{
+	private Plano planoContratado;
+	private LocalDate dataMatricula;
+	private List<Avaliacao> avaliacoesFisicas = new ArrayList<>();
+	
+	public Aluno(Plano plano, LocalDate dataMatricula) {
+		this.plano = plano;
+		this.dataMatricula = dataMatricula;
+	}
 }
