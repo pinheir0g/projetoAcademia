93f26de24c5101e6901e8f99b2d2b85e5bd0b241
João Marcelo <joaomarcelo2000@yahoo.com>
2024-04-10 16:27:33 -0300

    Inclusao das Classes Avaliacao e Agendamento

--- src/academia/classes/Aluno.java
+++ src/academia/classes/Aluno.java
@@ -9,8 +9,9 @@ public class Aluno extends Pessoa{
 	private LocalDate dataMatricula;
 	private List<Avaliacao> avaliacoesFisicas = new ArrayList<>();
 	
-	public Aluno(Plano plano, LocalDate dataMatricula) {
-		this.plano = plano;
+	public Aluno(String nome, String cpf, LocalDate dataNascimento, String contato, String senha, Plano planoContratado, LocalDate dataMatricula) {
+		super(nome, cpf, dataNascimento, contato, senha);
+		this.planoContratado = planoContratado;
 		this.dataMatricula = dataMatricula;
 	}
 }
