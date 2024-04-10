f18ee61f0b4b1fcc9b1ae344a59a3276d6327b38
LeoEsplinio <leoexplinio@gmail.com>
2024-04-10 16:36:37 -0300

    upDate class plano e funcionario

--- src/academia/classes/Funcionario.java
+++ src/academia/classes/Funcionario.java
@@ -1,5 +1,14 @@
 package academia.classes;
 
 public class Funcionario {
+	private String cargo;
 
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
 }
